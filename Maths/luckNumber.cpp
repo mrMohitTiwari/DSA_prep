9a52298a8f648ed3557b93e893d8c2a0703cc447
Mohit Tiwari <mt0988145@gmail.com>
2026-09-08 15:46:41 +0530

    adding files

--- Maths/luckNumber.cpp
+++ Maths/luckNumber.cpp
@@ -1,9 +1,29 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define endl '\n'
+#define int long long
+int a[56];
+void precompute1()
+{
 
+    a[0]=2;
+    for(int i=1;i<56;i++)
+    a[i]=a[i-1]*2;
+}
+void precompute2()
+{
+    for(int i =1;i<56;i++)
+        {
+            a[i]= a[i-1]+a[i];
+        }
+       
+}
 void solve(){
-    
+    int n;
+    cin>>n;
+    cout<<a[n-1]<<endl;
+
+
 }
 signed main() {
     // Fast I/O Magic Spell
@@ -12,8 +32,8 @@ signed main() {
     cout.tie(NULL);
 
     // Now you can take input safely
-    int _t=1;
-    cin >> _t;
+    int _t=1; precompute1();
+    precompute2();
     for(int i=0;i<_t;i++){
     solve();
     }
