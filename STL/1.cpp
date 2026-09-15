2b12cb0356e42f09d27210fff40d8516d26aa703
Mohit Tiwari <mt0988145@gmail.com>
2026-09-15 16:01:13 +0530

    adding files

--- STL/1.cpp
+++ STL/1.cpp
@@ -2,28 +2,33 @@
 using namespace std;
 #define endl '\n'
 
-void solve(){
+void solve()
+{
     // use of next_permutation
-    // let us create a vector and see what will be the next permutation 
+    // let us create a vector and see what will be the next permutation
     // vector<int> v= {1,2,3};
-    vector<int> v= {3,2,1}; //it will not have any next permutation so it will return false
-    do{
-        for(int x:v) cout<<x<<" ";
-        cout<<"\n";
+    vector<int> v = {3, 2, 1}; // it will not have any next permutation so it will return false
+    do
+    {
+        for (int x : v)
+            cout << x << " ";
+        cout << "\n";
 
-    }while(next_permutation(v.begin(),v.end()));
+    } while (next_permutation(v.begin(), v.end()));
 }
-signed main() {
+signed main()
+{
     // Fast I/O Magic Spell
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
 
     // Now you can take input safely
-    int _t=1;
+    int _t = 1;
     cin >> _t;
-    for(int i=0;i<_t;i++){
-    solve();
+    for (int i = 0; i < _t; i++)
+    {
+        solve();
     }
     return 0;
 }
--- STL/2.cpp
+++ STL/2.cpp
@@ -2,25 +2,27 @@
 using namespace std;
 #define endl '\n'
 
-void solve(){
+void solve()
+{
     // here will know how to use min_element() algo
-    vector<int> v = {1,2,3,45,0,-1};
-    auto it = min_element(v.begin(),v.end());
-    cout<<*it<<endl; //will give min element 
-    cout<<it - v.begin()<<endl; //it will return the index 
-
+    vector<int> v = {1, 2, 3, 45, 0, -1};
+    auto it = min_element(v.begin(), v.end());
+    cout << *it << endl;            // will give min element
+    cout << it - v.begin() << endl; // it will return the index
 }
-signed main() {
+signed main()
+{
     // Fast I/O Magic Spell
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
 
     // Now you can take input safely
-    int _t=1;
+    int _t = 1;
     // cin >> _t;
-    for(int i=0;i<_t;i++){
-    solve();
+    for (int i = 0; i < _t; i++)
+    {
+        solve();
     }
     return 0;
 }
